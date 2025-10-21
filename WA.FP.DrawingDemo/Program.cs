1cb5b765cb79c83db558baa067d994d9df5bb1ca
allen <g01.webapp@gmail.com>
2025-10-21 10:44:53 +0800

    初始化專案

---
+++ WA.FP.DrawingDemo/Program.cs
@@ -0,0 +1,10 @@
+namespace WA.FP.DrawingDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
